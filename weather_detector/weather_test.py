2344d982be9899a8da7f533596e30139141f6ee2
Dimitris Dedousis <dimitris.dedousis@gmail.com>
2018-10-20 15:30:17 +0300

    Working weather reporter

--- weather_detector/weather_test.py
+++ weather_detector/weather_test.py
@@ -1,12 +1,33 @@
 from weather import Weather, Unit
 
-weather = Weather(unit=Unit.CELSIUS)
-location = weather.lookup_by_latlng(53.3494,-6.2601)
+class WeatherReporter:
+    w = None
+    def __init__(self, unit):
+        self.w = Weather(unit)
 
-forecasts = location.forecast
-for forecast in forecasts:
-    print(forecast.text)
-    print("Date: " + forecast.date)
-    print("High temprature: " + forecast.high)
-    print("Low temprature: " + forecast.low)
-    print("")
+    def get_result_for_geolocation(self, lat, lon):
+        location = self.w.lookup_by_latlng(lat, lon)
+        condition = location.condition
+        wind_speed = location.wind.speed
+        wind_direction = location.wind.direction
+        return {"condition":condition.text, "windSpeed":wind_speed, "windDirection":wind_direction, "temp":location.condition.temp}
+
+    def get_forecasting_results_for_geolocation(self, lat, lon):
+        location = self.w.lookup_by_latlng(lat, lon)
+        forecasts = location.forecast
+        list = []
+        for forecast in forecasts:
+            list.append({"date":forecast.date, "high":forecast.high, "low":forecast.low, "description":forecast.text})
+        return list
+
+
+w = WeatherReporter(Unit.CELSIUS)
+list = w.get_forecasting_results_for_geolocation(53.3494, -6.2601)
+location = w.get_result_for_geolocation(53.3494, -6.2601)
+
+print("Description of the weather: " + location.get("condition"))
+
+for i in range(0,len(list)):
+    print("High temprature: " + list[i].get("high"))
+    print("Low temprature: " + list[i].get("high"))
+
